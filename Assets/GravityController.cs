6b5b8889e7c7364ab2f4ef8ae682d4c8393791a2
Char <charlesvader2@gmail.com>
2024-02-19 18:37:42 +0100

    spil

---
+++ Assets/GravityController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour {
+
+  private Rigidbody2D rb;
+    
+  void Start(){
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void Update()
+  {
+      if(Input.GetKeyDown(KeyCode.E)){
+        rb.gravityScale *= -1;
+
+      }
+    
+  }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Horizontal Movement Settings")]
+    [SerializeField] private float walkSpeed = 1;
+    
+    [Header("Ground Check Settings")]
+    [SerializeField] private float jumpForce= 15;
+    [SerializeField] private Transform GroundCheck;
+    [SerializeField] private float GroundCheckY = 0.2f;
+    [SerializeField] private float GroundCheckX = 0.5f;
+    [SerializeField] private LayerMask whatIsGround;
+
+    private Rigidbody2D rb;
+    private float xAxis;
+    private float xScale;
+
+    public static PlayerController Instance;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        xScale = transform.localScale.x;
+    }
+
+    void Update()
+    {
+        GetInputs();
+        Move(); 
+        Jump();
+        Flip();
+    }
+
+    void GetInputs()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    void Flip()
+    {
+        if(xAxis < 0)
+        {
+            transform.localScale = new Vector2(-xScale, transform.localScale.y);
+        }
+        else if(xAxis > 0)
+        {
+            transform.localScale = new Vector2(xScale, transform.localScale.y);
+        }
+    }
+
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(walkSpeed * xAxis, rb.velocity.y);
+    }
+
+    public bool Grounded()
+    {
+        if (Physics2D.Raycast(GroundCheck.position, Vector2.down, GroundCheckY, whatIsGround)
+             || Physics2D.Raycast(GroundCheck.position + new Vector3(GroundCheckX, 0, 0), Vector2.down, GroundCheckY, whatIsGround)
+             || Physics2D.Raycast(GroundCheck.position + new Vector3(-GroundCheckX, 0, 0), Vector2.down, GroundCheckY, whatIsGround))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
+        
+        if (Input.GetButtonDown("Jump") && Grounded())
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce);
+        }
+    }
+
+}
